0b399d7a7e4dabce2d523c59ae1f2fcfb20fbd0c
Spinozist <ewoodworth@atlantaregional.org>
2018-12-05 14:21:03 -0500

    fixed lint issues

--- models/Art.js
+++ models/Art.js
@@ -1,38 +1,42 @@
 module.exports = function(sequelize, DataTypes) {
-  var Art = sequelize.define("Art", {
-    name: DataTypes.STRING,
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        min: 5
+  var Art = sequelize.define(
+    "Art",
+    {
+      name: DataTypes.STRING,
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          min: 5
+        }
+      },
+      artist: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          max: 50
+        }
+      },
+      category: {
+        type: DataTypes.STRING,
+        defaultValue: "Uncategorized",
+        allowNull: false
+      },
+      latitude: DataTypes.FLOAT(11,7),
+      longitude: DataTypes.FLOAT(11,7),
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: sequelize.literal("NOW()")
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: sequelize.literal("NOW()")
       }
     },
-    artist: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      validate: {
-        max: 50
-      }
-    },
-    category: {
-      type: DataTypes.STRING,
-      defaultValue: "Uncategorized",
-      allowNull: false
-    },
-    latitude: DataTypes.FLOAT(11,7),
-    longitude: DataTypes.FLOAT(11,7),
-    createdAt: {
-      type: DataTypes.DATE,
-      defaultValue: sequelize.literal("NOW()")
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      defaultValue: sequelize.literal("NOW()")
+    {
+      timestamps: true
     }
-  }, {
-    timestamps: true
-  });
+  );
 
   Art.associate = function(models) {
     Art.belongsTo(models.User, {
